40157e3fbe6dbc8cfb76c62ba759c6c1f69a3bed
Logan Anderson <logananderson248@gmail.com>
2020-07-19 22:06:53 +0000

    Fixed errors

--- scripts/install.sh
+++ scripts/install.sh
@@ -20,44 +20,43 @@ completed(){
   fi
 }
 
-function install(){
-  # Check if program is already installed
-  dpkg -s $1 &> /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Already installed: ${1}"
-  else
-    echo "Installing: ${1}..."
-    pkg uninstall $1 -y
-  fi
-}
+# function install(){
+#   # Check if program is already installed
+#   dpkg -s $1 &> /dev/null
+#   if [ $? -eq 0 ]; then
+#     echo "Already installed: ${1}"
+#   else
+#     echo "Installing: ${1}..."
+#     pkg install $1 -y
+#   fi
+# }
 
 upgrade_packages
-install clang
-install aria2
-install htop
-install tree
-install python
-install neovim
-install ncurses-utils
-install nodejs-lts
-install ctags
-install fd
-install ripgrep
-install fzf
-install ranger
-install fontconfig-utils
-install rsync
-install dnsutils
-install man
-install ffmpeg
-install mpv # vidro player
-install cmus # music player
-install vifm # file manager
-install mc # file manager
 
-#Fun stuff
-install figlet
-install toilet
+pkg install clang \
+  aria2 \
+  htop \
+  tree \
+  python \
+  neovim \
+  ncurses-utils \
+  nodejs-lts \
+  ctags \
+  fd \
+  ripgrep \
+  fzf \
+  ranger \
+  fontconfig-utils \
+  rsync \
+  dnsutils \
+  man \
+  ffmpeg \
+  mpv \
+  cmus \
+  vifm \
+  mc \
+  figlet \
+  toilet
 
 if [ -f ./setup.sh ]; then
   ./setup.sh
